e6a86bbfbf6ccc09fd90823902b471b4254898ca
Xxviarrah-silentxX <74418860+Xxviarrah-silentxX@users.noreply.github.com>
2020-12-11 19:15:53 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class Box{
+constructor(){
+
+    var options= {
+  // boxpart:isStatic = true,
+  // boxpart2:isStatic = true,
+  // boxpart3:isStatic = true
+    }
+
+
+
+   
+
+
+    boxpart=createSprite(width/2-100,610,20,100);
+	boxpart.shapeColor = "red";
+
+    boxpart2=createSprite(width/2,650,200,20);
+	boxpart2.shapeColor = "red";
+
+    boxpart3=createSprite(width/2+100,610,20,100);
+	boxpart3.shapeColor = "red";
+}
+
+
+
+
+}
